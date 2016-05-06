ce6be42412acfaa0b5fb3b2b4c33a7e470451812
Davy MELINA <davy.melina@gmail.com>
2016-05-06 12:24:16 +0200

    Prepare images call

---
+++ images.go
@@ -0,0 +1,9 @@
+package scaleway
+
+// ImagesService handles communication with the images related
+// methods of the Scaleway API.
+//
+// Scaleway API docs: https://developer.scaleway.com/#images
+type ImagesService struct {
+	client *Client
+}
