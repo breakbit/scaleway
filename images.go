429204233fa2cce7f490da1d4bb5a9d7f4c6eba6
Davy MELINA <davy.melina@gmail.com>
2016-05-09 14:58:47 +0200

    Add images call

--- images.go
+++ images.go
@@ -12,18 +12,18 @@ type ImagesService struct {
 
 // Images represents a Scaleway images.
 type Image struct {
-	CreationDate     Ntime    `json:"creation_date,omitempty"`
-	ModificationDate Ntime    `json:"modification_date,omitempty"`
-	Arch             string   `json:"arch,omitempty"`
-	ExtraVolumes     []string `json:"extra_volumes,omitempty"`
-	FromImage        string   `json:"from_image,omitempty"`
-	FromServer       string   `json:"from_server,omitempty"`
-	ID               string   `json:"id,omitempty"`
-	MarketplaceKey   string   `json:"markeplace_key,omitempty"`
-	Name             string   `json:"name,omitempty"`
-	Organization     string   `json:"organization,omitempty"`
-	Public           bool     `json:"public,omitempty"`
-	RootVolume       *Volume  `json:"root_volume,omitempty"`
+	CreationDate     Ntime  `json:"creation_date,omitempty"`
+	ModificationDate Ntime  `json:"modification_date,omitempty"`
+	Arch             string `json:"arch,omitempty"`
+	//ExtraVolumes     []string `json:"extra_volumes,omitempty"`
+	FromImage      string  `json:"from_image,omitempty"`
+	FromServer     string  `json:"from_server,omitempty"`
+	ID             string  `json:"id,omitempty"`
+	MarketplaceKey string  `json:"markeplace_key,omitempty"`
+	Name           string  `json:"name,omitempty"`
+	Organization   string  `json:"organization,omitempty"`
+	Public         bool    `json:"public,omitempty"`
+	RootVolume     *Volume `json:"root_volume,omitempty"`
 }
 
 // ImageRequest represents a request to create a image.
@@ -58,4 +58,54 @@ func (s *ImagesService) Create(tr *ImageRequest) (*Image, *Response, error) {
 		return nil, nil, err
 	}
 	return image.Image, resp, nil
+}
+
+// List returns a list of all images.
+func (s *ImagesService) List() ([]*Image, *Response, error) {
+	return s.listImages()
+}
+
+func (s *ImagesService) listImages() ([]*Image, *Response, error) {
+	u := fmt.Sprintf("/images")
+	req, err := s.client.NewRequestAccount("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	images := new(imageListResponse)
+	resp, err := s.client.Do(req, images)
+	if err != nil {
+		return nil, nil, err
+	}
+	return images.Images, resp, nil
+}
+
+// Get returns info for a specific image.
+func (s *ImagesService) Get(id string) (*Image, *Response, error) {
+	u := fmt.Sprintf("/images/%s", id)
+	req, err := s.client.NewRequestAccount("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	image := new(imageResponse)
+	resp, err := s.client.Do(req, image)
+	if err != nil {
+		return nil, nil, err
+	}
+	return image.Image, resp, nil
+}
+
+// Delete deletes a image.
+func (s *ImagesService) Delete(id string) (*Response, error) {
+	u := fmt.Sprintf("/images/%s", id)
+	req, err := s.client.NewRequestAccount("DELETE", u, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := s.client.Do(req, nil)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
--- images_test.go
+++ images_test.go
@@ -65,3 +65,112 @@ func TestImagesService_Create(t *testing.T) {
 		t.Errorf("Images.Create returned %+v\n, want %+v", image, want)
 	}
 }
+
+func TestImagesService_List(t *testing.T) {
+	setup()
+	defer teardown()
+
+	client.AuthToken = "654c95b0-2cf5-41a3-b3cc-733ffba4b4b7"
+
+	data := testOpenFixture(t, filepath.Join(fixtureDir, "images_list.json"))
+
+	mux.HandleFunc("/images", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		w.Header().Add("Content-Type", contentType)
+		fmt.Fprint(w, string(data))
+	})
+
+	images, _, err := client.Images.List()
+	if err != nil {
+		t.Errorf("Images.List returned error: %v", err)
+	}
+
+	creationDate, _ := time.Parse("2006-01-02T15:04:05.999999Z07:00", "2014-05-22T12:56:56.984011+00:00")
+	modificationDate, _ := time.Parse("2006-01-02T15:04:05.999999Z07:00", "2014-05-22T12:56:56.984011+00:00")
+
+	want := []*Image{
+		{
+			CreationDate:     Ntime(creationDate),
+			ModificationDate: Ntime(modificationDate),
+			Arch:             "arm",
+			ID:               "98bf3ac2-a1f5-471d-8c8f-1b706ab57ef0",
+			//ExtraVolumes:     []string{},
+			FromImage:      "",
+			FromServer:     "",
+			MarketplaceKey: "",
+			Name:           "my_image",
+			Organization:   "000a115d-2852-4b0a-9ce8-47f1134ba95a",
+			Public:         false,
+			RootVolume: &Volume{
+				Name: "vol-0-1",
+				ID:   "f0361e7b-cbe4-4882-a999-945192b7171b",
+			},
+		},
+	}
+	if !reflect.DeepEqual(images, want) {
+		t.Errorf("Images.List returned %+v\n, want %+v", images, want)
+	}
+}
+
+func TestImagesService_Get(t *testing.T) {
+	setup()
+	defer teardown()
+
+	client.AuthToken = "654c95b0-2cf5-41a3-b3cc-733ffba4b4b7"
+
+	data := testOpenFixture(t, filepath.Join(fixtureDir, "images_get.json"))
+
+	creationDate, _ := time.Parse("2006-01-02T15:04:05.999999Z07:00", "2014-05-22T12:56:56.984011+00:00")
+	modificationDate, _ := time.Parse("2006-01-02T15:04:05.999999Z07:00", "2014-05-22T12:56:56.984011+00:00")
+
+	want := &Image{
+		CreationDate:     Ntime(creationDate),
+		ModificationDate: Ntime(modificationDate),
+		Arch:             "arm",
+		ID:               "98bf3ac2-a1f5-471d-8c8f-1b706ab57ef0",
+		//ExtraVolumes:     []string{},
+		FromImage:      "",
+		FromServer:     "",
+		MarketplaceKey: "",
+		Name:           "my_image",
+		Organization:   "000a115d-2852-4b0a-9ce8-47f1134ba95a",
+		Public:         false,
+		RootVolume: &Volume{
+			Name: "vol-0-1",
+			ID:   "f0361e7b-cbe4-4882-a999-945192b7171b",
+		},
+	}
+
+	mux.HandleFunc(fmt.Sprintf("/images/%s", want.ID), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		w.Header().Add("Content-Type", contentType)
+		fmt.Fprint(w, string(data))
+	})
+
+	image, _, err := client.Images.Get(want.ID)
+	if err != nil {
+		t.Errorf("Images.Get returned error: %v", err)
+	}
+	if !reflect.DeepEqual(image, want) {
+		t.Errorf("Images.Get returned %+v\n, want %+v", image, want)
+	}
+}
+
+func TestImagesService_Delete(t *testing.T) {
+	setup()
+	defer teardown()
+
+	client.AuthToken = "654c95b0-2cf5-41a3-b3cc-733ffba4b4b7"
+
+	imageID := "654c95b0-2cf5-41a3-b3cc-733ffba4b4b7"
+
+	mux.HandleFunc(fmt.Sprintf("/images/%s", imageID), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+		w.Header().Add("Content-Type", contentType)
+	})
+
+	_, err := client.Images.Delete(imageID)
+	if err != nil {
+		t.Errorf("Images.Delete returned error: %v", err)
+	}
+}
