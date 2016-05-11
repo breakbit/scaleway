a59be460c52abbb58ef2a9efa6607b3123c0fe01
Davy MELINA <davy.melina@gmail.com>
2016-05-11 17:05:16 +0200

    Prepare IPs service

---
+++ ips.go
@@ -0,0 +1,9 @@
+package scaleway
+
+// IPsService handles communication with the servers related
+// methods of the Scaleway API.
+//
+// Scaleway API docs: https://developer.scaleway.com/#ips
+type IPsService struct {
+	client *Client
+}
